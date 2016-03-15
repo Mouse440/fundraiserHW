8cf95ea8c5d27d6df7a0b78c0d57237cc895c49b
Ubuntu <ubuntu@ip-172-31-32-51.us-west-2.compute.internal>
2016-03-15 20:20:30 +0000

    part1 readFile done

---
+++ part1/readFile/index.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var fs = require('fs');
+
+fs.readFile('./index.html',function(err, data){
+	if(err)
+		throw err;
+	else {
+		var requestListener = function (req, res) {
+ 			res.writeHead(200, {'Content-Type': 'text/html'});
+  			res.write(data);
+			res.end();
+		}
+		var server = http.createServer(requestListener);
+		server.listen(process.env.PORT || 8080);	
+	}
+} );
